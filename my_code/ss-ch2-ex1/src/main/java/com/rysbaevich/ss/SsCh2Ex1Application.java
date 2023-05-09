0c328cd296e4930721f25829b54138c07230f767
Нурдөөлөт <r.nurdoolot@gmail.com>
2023-05-09 15:15:03 +0600

    First commit

---
+++ my_code/ss-ch2-ex1/src/main/java/com/rysbaevich/ss/SsCh2Ex1Application.java
@@ -0,0 +1,13 @@
+package com.rysbaevich.ss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsCh2Ex1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SsCh2Ex1Application.class, args);
+    }
+
+}
